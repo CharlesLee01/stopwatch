82fcb219798d39f4f1db0ecb422d2503d4e0764a
Charles Lee Lin Ta <e0484321@u.nus.edu>
2023-10-20 22:28:48 +0800

    first commit

---
+++ stopwatch.js
@@ -0,0 +1,50 @@
+let second = 0;
+let minute = 0;
+let intervalID;
+let isStart;
+
+const startButton = document.querySelector('.js-start-button');
+const resetButton = document.querySelector('.js-reset-button');
+const time = document.querySelector('.js-time');
+
+startButton.addEventListener('click', () => start());
+resetButton.addEventListener('click', () => reset());
+
+function start() {
+	if (!isStart) {
+		isStart = true;
+		intervalID = setInterval(() => {
+			displayTime();
+			second++;
+		}, 1000);
+		startButton.innerHTML = 'Stop';
+	} else {
+		console.log('hi');
+		isStart = false;
+		clearInterval(intervalID);
+		startButton.innerHTML = 'Start';
+	}	
+}
+
+function displayTime() {
+	second = String(second % 60);
+	minute = String(Math.floor(second / 60));
+
+	if (second.length === 1) {
+		second = '0' + second; 
+	}
+
+	if (minute.length === 1) {
+		minute = '0' + minute; 
+	}
+	time.innerHTML = `${minute} : ${second}`;
+}
+
+function reset() {
+	if (isStart) {
+		isStart = false;
+		clearInterval(intervalID);
+	}
+  second = 0;
+	displayTime();
+}
